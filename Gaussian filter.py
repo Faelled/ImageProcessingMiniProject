2e489b46c4621caee9d959fe28d903bba580105a
BurmasGG <guitartard@hotmail.com>
2020-04-23 11:41:19 +0200

    some comments

--- Gaussian filter.py
+++ Gaussian filter.py
@@ -2,33 +2,32 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread("aau-city-2.jpg", 0)
+img = cv2.imread("aau-city-2.jpg", 0)#Make sure image is grayscale
 cv2.imshow('image1', img)
 #cv2.imshow('image', img)
 
-mean = [1,1,1,1,1,1,1,1,1]
 
 def GaussFilter(img):
     dims = img.shape
-    x = dims[0]
-    y = dims[1]
-    kernelSize = 3
-    offset = kernelSize//2
+    x = dims[0] #Columns
+    y = dims[1] #Rows
+    kernelSize = 3 
+    offset = kernelSize//2 
     gaussKernel= np.array([[1,2,1],
                   [2,4,2],
                   [1,2,1]])
        
        
-    for j in range (offset, y-offset):
+    for j in range (offset, y-offset): #Loop through every pixel in the image, the offset is used to avoid the edges of the image
         for i in range (offset, x-offset):
             newValue = 0.0
             
-            for l in range(0, kernelSize):
+            for l in range(0, kernelSize):#Loop through the kernels rows and columns
                 for k in range(0, kernelSize):
-                    newValue += img[i+k-offset][j+l -offset]*gaussKernel[l][k]
+                    newValue += img[i+k-offset][j+l -offset]*gaussKernel[l][k] 
   
-            newValue=newValue//(16)
-            img[i][j] = newValue
+            newValue=newValue//(16)#16 is sum of the kernel
+            img[i][j] = newValue #Set the pixel to the new value calculated for it
             
     return img;
 
